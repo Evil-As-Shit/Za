6473a251e60ef8098967e0026fe46bf97909b80d
Anthony <messinabrothers@gmail.com>
2024-01-18 10:01:30 -0800

    refactor: Replaced get_node("NodeName") with $NodeName

--- Scripts/Worker.gd
+++ Scripts/Worker.gd
@@ -17,8 +17,8 @@ var is_debug_print: bool = false
 func _ready():
 	
 	SignalController.debug_walk_to.connect(self._on_debug_walk_to)
-	get_node("Area2D").connect("mouse_entered", self.on_mouse_entered.bind(true))
-	get_node("Area2D").connect("mouse_exited", self.on_mouse_entered.bind(false))
+	$Area2D.connect("mouse_entered", self.on_mouse_entered.bind(true))
+	$Area2D.connect("mouse_exited", self.on_mouse_entered.bind(false))
 
 func set_current_state(new_state):
 	if(new_state == current_state):
@@ -52,4 +52,4 @@ func _on_debug_walk_to(pos:Vector2):
 	nav_agent.target_position = pos
 
 func on_mouse_entered(b:bool):
-	SignalController.emit_signal("hover_over", get_node("Area2D"), b)
+	SignalController.emit_signal("hover_over", $Area2D, b)
