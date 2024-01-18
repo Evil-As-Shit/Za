62a7677478fe353ec503db8912152a268cc470a1
Anthony <messinabrothers@gmail.com>
2024-01-18 09:29:21 -0800

    debug: Utility.dir(class_instance) gets the methods and properties of a class.

--- Scripts/Utility.gd
+++ Scripts/Utility.gd
@@ -4,3 +4,20 @@ static func is_at(pos1:Vector2, pos2:Vector2, margin:float = 1.0) -> bool:
 	var diffVector = Vector2(pos2.x - pos1.x, pos2.y - pos1.y)
 	var squaredDistance = diffVector[0]*diffVector[0] + diffVector[1]*diffVector[1]
 	return squaredDistance < (margin * margin)
+
+# debug: gets the methods and properties of a class
+static func dir(class_instance):
+	var output = {}
+	var methods = []
+	for method in class_instance.get_method_list():
+		methods.append(method.name)
+	
+	output["METHODS"] = methods
+	
+	var properties = []
+	for prop in class_instance.get_property_list():
+		if prop.type == 3:
+			properties.append(prop.name)
+	output["PROPERTIES"] = properties
+
+	return output
