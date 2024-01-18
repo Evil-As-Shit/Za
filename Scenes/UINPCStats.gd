13e9f28c0a57050d881e7f669dec291c059863c7
Anthony <messinabrothers@gmail.com>
2024-01-18 05:41:13 -0800

    feat: UI shows worker stats like clean, cook, and hospitality.

--- Scenes/UINPCStats.gd
+++ Scenes/UINPCStats.gd
@@ -1,4 +1,10 @@
 extends UINPC
 
 func _update(id:int):
-	print("updating stats of ",id)
+	$Label.text = str(
+			"Clean:",GameData.get_worker_stat(id, "abil_clean"),"\n",
+			"Cook:",GameData.get_worker_stat(id, "abil_cook"),"\n",
+			"Hosp:",GameData.get_worker_stat(id, "abil_hospitable"),"\n",
+			"Stamina:",GameData.get_worker_stat(id, "abil_stamina"),"\n",
+			)
+	
