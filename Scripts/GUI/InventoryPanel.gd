c64d60b87e947d12bf18da97a42199b0a8f5597d
Anthony <messinabrothers@gmail.com>
2024-01-20 09:02:27 -0800

    refactor: Renamed variable to get rid of Godot warning.

--- Scripts/GUI/InventoryPanel.gd
+++ Scripts/GUI/InventoryPanel.gd
@@ -5,7 +5,5 @@ func _ready():
 	update(randi() % 4,1)
 
 func update(x:float, y:float):
-	var size:float = 64.0
-	#$TextureRect.texture_normal_region = Rect2(x * size, y * size, size, size)
-	#$TextureRect.region_rect = Rect2(x * size, y * size, size, size)
-	$Sprite2D.region_rect = Rect2(x * size, y * size, size, size)
+	var texture_size:float = 64.0
+	$Sprite2D.region_rect = Rect2(x * texture_size, y * texture_size, texture_size, texture_size)
